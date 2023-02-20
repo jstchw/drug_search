b4e73eb03b4a3ed427115c4b63581ee38c0025a4
jstchw <bobr703@gmail.com>
2023-02-20 17:22:57 +0000

    Clean up package lock and add yarn lock

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    const [currentTime, setCurrentTime] = useState(null);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            fetch('/api/time').then(res => res.json()).then(data => {
+                const date = new Date(data.time * 1000);
+                setCurrentTime(date.toLocaleTimeString());
+            });
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );

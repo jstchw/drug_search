76eadfcb44258021a705ed1ace841ec420196764
jstchw <bobr703@gmail.com>
2023-07-09 20:51:00 +0100

    Correct information when a drug is clicked in the wordcloud

--- src/components/DemographicModal/DemographicModal.jsx
+++ src/components/DemographicModal/DemographicModal.jsx
@@ -4,16 +4,20 @@ import {useEffect, useState} from "react";
 import DrugDescription from "../DrugDescription/DrugDescription";
 import DrugAccordion from "../DrugAccordion/DrugAccordion";
 import useDrugInfo from "../../hooks/useDrugInfo";
-import {searchTypes} from "../OptionModal/OptionModal";
+import {getDrugsFromEvents, getEventsFromDrugs, getEventsOverTime} from "../../services/FDA_Request";
+import { searchTypes } from "../OptionModal/OptionModal";
 
-const DemographicModalInfo = ({word, show}) => {
+const DemographicModalInfo = ({word}) => {
     const drugInfo = useDrugInfo([word], searchTypes[0].value)
 
     return (
         <div>
             {drugInfo && drugInfo.length > 0 &&
                 <React.Fragment>
-                    <DrugDescription drugInfo={drugInfo[0]} showAdditionalSearch={false}/>
+                    <DrugDescription
+                        drugInfo={drugInfo[0]}
+                        style={{border: "none"}}
+                    />
                     <DrugAccordion drugInfo={drugInfo[0]}/>
                 </React.Fragment>
             }
@@ -24,15 +28,13 @@ const DemographicModalInfo = ({word, show}) => {
 const DemographicModal = ({ show, handleClose, selectedWord}) => {
     const [word, setWord] = useState(null)
 
-    // Make the word not all caps
+    // Make the word not all caps and check if the word has changed
     useEffect(() => {
         if (selectedWord) {
-            const newWord = selectedWord.charAt(0).toUpperCase() + selectedWord.slice(1).toLowerCase()
-            if(newWord !== word) {
-                setWord(newWord)
-            }
+            const newWord = selectedWord.charAt(0).toUpperCase() + selectedWord.slice(1).toLowerCase();
+            setWord(prevWord => newWord !== prevWord ? newWord : prevWord);
         }
-    }, [selectedWord])
+    }, [selectedWord]);
 
     const handleModalClose = () => {
         handleClose()
--- src/components/DrugDescription/DrugDescription.jsx
+++ src/components/DrugDescription/DrugDescription.jsx
@@ -1,40 +1,23 @@
 import React from 'react';
-import {Capsule as CapsuleIcon} from "react-bootstrap-icons";
 import { Card, Badge } from 'react-bootstrap';
 import './DrugDescription.css';
 
 
-const DrugDescription = ({ drugInfo, showAdditionalSearch }) => {
-
-    const dualSearchRef = React.useRef(showAdditionalSearch);
-
+const DrugDescription = (props) => {
     return (
-        <Card className={'drug-description-card mb-4'}>
-            <Card.Header>
-                <CapsuleIcon className="drug-badge mx-1" />
-                {dualSearchRef.current ? (
-                    <>
-                        <span className={'drug-name'}>{drugInfo.drug_name}</span>
-                    </>
-                ) : (
-                    <>
-                        <span className={'drug-name'}>{'Molecule'}</span>
-                    </>
-                )}
-            </Card.Header>
-
-            {drugInfo.molecule_url && <Card.Img src={drugInfo.molecule_url} alt={drugInfo.drug_name}
+        <Card className={'drug-description-card mb-4'} style={props.style}>
+            {props.drugInfo.molecule_url && <Card.Img src={props.drugInfo.molecule_url} alt={props.drugInfo.drug_name}
                                                 className="molecule" style={{ backgroundColor: 'transparent'}} />}
 
             <Card.Body className="text-md-start">
-                {drugInfo && drugInfo.iupac &&
-                    <p><Badge>IUPAC</Badge>&nbsp;<span className={'iupac'}>{drugInfo.iupac}</span></p>
+                {props.drugInfo && props.drugInfo.iupac &&
+                    <p><Badge>IUPAC</Badge>&nbsp;<span className={'iupac'}>{props.drugInfo.iupac}</span></p>
                 }
-                {drugInfo && drugInfo.drug_class &&
-                    <p><Badge>Class</Badge>&nbsp;{drugInfo.drug_class}</p>
+                {props.drugInfo && props.drugInfo.drug_class &&
+                    <p><Badge>Class</Badge>&nbsp;{props.drugInfo.drug_class}</p>
                 }
-                {drugInfo && drugInfo.formula &&
-                    <p><Badge>Formula</Badge>&nbsp;{drugInfo.formula}</p>
+                {props.drugInfo && props.drugInfo.formula &&
+                    <p><Badge>Formula</Badge>&nbsp;{props.drugInfo.formula}</p>
                 }
             </Card.Body>
         </Card>

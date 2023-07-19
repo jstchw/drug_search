1e673cda3020385b875600c8cf2da70a4878dea5
jstchw <bobr703@gmail.com>
2023-07-19 19:04:22 +0200

    Small cookie banner animation added

--- src/components/CookieInfoFooter/CookieInfoFooter.jsx
+++ src/components/CookieInfoFooter/CookieInfoFooter.jsx
@@ -1,6 +1,7 @@
-import {Button, Col, Container, Row} from "react-bootstrap";
+import {Button, Col, Row} from "react-bootstrap";
 import {useEffect, useState} from "react";
 import './CookieInfoFooter.css'
+import {CSSTransition} from "react-transition-group";
 
 export const CookieInfoFooter = () => {
     const [show, setShow] = useState(false)
@@ -17,18 +18,22 @@ export const CookieInfoFooter = () => {
         setShow(false)
     }
 
-    if (!show) {
-        return null
-    }
-
     return (
-        <div className={'cookie-info-footer fixed-bottom d-flex justify-content-center m-4 text-center'}>
-            <Row>
-                <Col md="auto" className={'p-3'}>
-                    We use cookies for functionality, no personal data is collected.
-                    <Button className={'mx-2'} onClick={handleDismissed}>Got it!</Button>
-                </Col>
-            </Row>
-        </div>
+        <CSSTransition
+            in={show}
+            timeout={300}
+            classNames="fade"
+            unmountOnExit
+        >
+            { show ? (
+            <div className={'cookie-info-footer fixed-bottom d-flex justify-content-center m-4 text-center'}>
+                <Row>
+                    <Col md="auto" className={'p-3'}>
+                        We use cookies for functionality, no personal data is collected.
+                        <Button className={'mx-2'} onClick={handleDismissed}>Got it!</Button>
+                    </Col>
+                </Row>
+            </div> ) : <></> }
+        </CSSTransition>
     )
 }

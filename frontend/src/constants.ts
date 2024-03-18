eaeb10855e10f8052db4b7f405770f0031b35bbe
hybrid_theory <bobr703@gmail.com>
2024-03-18 22:31:25 +0000

    Interlink between services changed and favicon fixed for prod

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -294,7 +294,7 @@ export const drugGroupConfig: DrugGroupConfig = {
 
 export const backendUrl = `${window.location.protocol}//${window.location.hostname}:16000/api`;
 
-export const annotateFrontendUrl = `${window.location.protocol}//${window.location.hostname}:3001/`;
+export const annotateFrontendUrl = import.meta.env.DEV ? `${window.location.protocol}//${window.location.hostname}:3001/` : 'https://annotate.drugwatch.net/';
 
 export const baseFdaUrl = 'https://api.fda.gov/drug/event.json';
 
